f62a407c69094c479d4c827534e6c45a0ff6fe8e
vm93256 <vm93256@gmail.com>
2025-02-12 16:43:42 +0530

    adding functionality for multiple numbers sepearated with comma

--- src/test/java/com/StringCalculatorTest.java
+++ src/test/java/com/StringCalculatorTest.java
@@ -1,15 +1,31 @@
 package com;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StringCalculatorTest {
+
     @Test
     public void testSingleAddNumber() {
-        StringCalculator simpleCalculator = new StringCalculator();
-        assertEquals(0, simpleCalculator.add(""));
-        assertEquals(1, simpleCalculator.add("1"));
-        assertEquals(2, simpleCalculator.add("2"));
+        StringCalculator stringCalculator = new StringCalculator();
+        assertEquals(0, stringCalculator.add(""));
+        assertEquals(1, stringCalculator.add("1"));
+        assertEquals(2, stringCalculator.add("2"));
     }
+
+    @Test
+    public void testAddMultipleNumbers() {
+        StringCalculator stringCalculator = new StringCalculator();
+        assertEquals(3, stringCalculator.add("1,2"));
+    }
+
+    @Test
+    public void testEmptyString() {
+        StringCalculator stringCalculator = new StringCalculator();
+        assertEquals(0, stringCalculator.add(""));
+    }
+
 }
